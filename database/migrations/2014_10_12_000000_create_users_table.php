ad3408a3edc3d9085e86aed10568aee7dc71a57f
Andrés <ernestoandresrosillo@gmail.com>
2024-01-18 10:03:25 +0100

    Migrations correction

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('last_name');
-            $table->boolean('is_admin');
+            $table->boolean('is_admin')->default(false);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
--- database/migrations/2024_01_16_104922_create_topics_table.php
+++ database/migrations/2024_01_16_104922_create_topics_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('topics', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('topic_text');
             $table->timestamps();
         });
 
